f876e7701380105314db09368b15ff296965ff75
George Fedoseev <george.fedoseev@me.com>
2018-04-01 12:44:41 +0300

    print flag

--- DeepSpeech.py
+++ DeepSpeech.py
@@ -40,19 +40,25 @@ from telegram.ext import Updater
 from pprint import pformat
 updater = Updater("592335153:AAEDnx7bFAfW87znwH6tAYsAfS-JZwdJEy8")   
 def log_telegram(msg):
-    updater.bot.send_message(chat_id="79735423", text=msg)
+    try:
+        updater.bot.send_message(chat_id="79735423", text=msg)
+    except:
+        pass
 
 def telegram_send_text_as_attachement(name, text):
-    fname = name+".txt"
-    f = open(fname, "w")
-    f.write(text)    
-    f.close()
+    try:
+        fname = name+".txt"
+        f = open(fname, "w")
+        f.write(text)    
+        f.close()
 
-    f = open(fname, "r")
-    updater.bot.send_document(chat_id="79735423", document=f)
-    f.close()
+        f = open(fname, "r")
+        updater.bot.send_document(chat_id="79735423", document=f)
+        f.close()
 
-    os.remove(fname)
+        os.remove(fname)
+    except:
+        pass
 
 
 tf.logging.set_verbosity(tf.logging.ERROR)
