fe21a85ed4235de3ac00dd512f74cca765abab29
George Fedoseev <george.fedoseev@me.com>
2018-04-15 14:53:35 +0300

    fix

--- DeepSpeech.py
+++ DeepSpeech.py
@@ -1669,17 +1669,17 @@ def train(server=None):
                             log_info('Training epoch %i...' % COORD._epoch)
                             total_jobs = COORD._num_jobs_train
                             total_samples = model_feeder.train.total_batches*FLAGS.train_batch_size
-                            log_info("Total samples: %i", total_samples)
+                            log_info("Total samples: %i" % total_samples)
                         elif job.set_name == "dev":
                             log_info('Validating epoch %i...' % COORD._epoch)
                             total_jobs = COORD._num_jobs_dev
                             total_samples = model_feeder.dev.total_batches*FLAGS.dev_batch_size
-                            log_info("Total samples: %i", total_samples)
+                            log_info("Total samples: %i" % total_samples)
                         elif job.set_name == "test":
                             log_info('Testing epoch %i...' % COORD._epoch)
                             total_jobs = COORD._num_jobs_test
                             total_samples = model_feeder.test.total_batches*FLAGS.test_batch_size
-                            log_info("Total samples: %i", total_samples)
+                            log_info("Total samples: %i" % total_samples)
 
 
                         
