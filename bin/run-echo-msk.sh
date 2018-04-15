46ec4c3fb6f0d066d0b2a72f290881885868de36
George Fedoseev <george.fedoseev@me.com>
2018-04-15 12:05:02 +0300

    update echo msk train scriupt

--- bin/run-echo-msk.sh
+++ bin/run-echo-msk.sh
@@ -11,19 +11,24 @@ python -u DeepSpeech.py \
   --train_files "/tmp/echo-msk-dataset/echo-msk-train.csv" \
   --dev_files "/tmp/echo-msk-dataset/echo-msk-dev.csv" \
   --test_files "/tmp/echo-msk-dataset/echo-msk-test.csv" \
-  --train_batch_size 12 \
-  --dev_batch_size 12 \
-  --test_batch_size 12 \
+  --train_batch_size 32 \
+  --dev_batch_size 16 \
+  --test_batch_size 16 \
   --learning_rate 0.0001 \
-  --epoch 30 \
+  --epoch 35 \
   --display_step 0 \
-  --validation_step 5 \
-  --dropout_rate 0.30 \
+  --validation_step 1 \
+  --dropout_rate 0.20 \
   --default_stddev 0.046875 \
   --n_hidden 1024 \
-  --checkpoint_dir /assets/network/checkpoint_dir_echo_msk_1024/ \
-  --export_dir /assets/network/export_dir_echo_msk_1024/ \
-  --log_level 0 \
-  --limit_test 1500 \
-  --report_count 100 \
+  --checkpoint_dir /assets/network/echo_msk/checkpoint_dir_echo_msk_1024/ \
+  --export_dir /assets/network/echo_msk/export_dir_echo_msk_1024/ \
+  --summary_dir /assets/network/echo_msk/summary_echo_msk \
+  --summary_sec 60 \
+  --log_level 1 \
+  --limit_train 160000000  \
+  --report_count 1000 \
+  --xla=False \
+  --use_warpctc=True \
+  --log_telegram=True \
   "$@"
