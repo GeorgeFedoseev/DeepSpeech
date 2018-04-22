283cdbec7d147cf596b7df5fa7958966c209b7d8
George Fedoseev <george.fedoseev@me.com>
2018-04-23 02:12:58 +0300

    try 2500

--- bin/train-yt-vad-autosubs-rus.sh
+++ bin/train-yt-vad-autosubs-rus.sh
@@ -20,9 +20,9 @@ python -u DeepSpeech.py \
   --validation_step 1 \
   --dropout_rate 0.30 \
   --default_stddev 0.046875 \
-  --n_hidden 4096 \
-  --checkpoint_dir /assets/network/yt-vad-autosubs-4096/yt-vad-autosubs-4096-checkpoints/ \
-  --export_dir /assets/network/yt-vad-autosubs-4096/yt-vad-autosubs-4096-export/ \
+  --n_hidden 2500 \
+  --checkpoint_dir /assets/network/yt-vad-autosubs-2500/yt-vad-autosubs-2500-checkpoints/ \
+  --export_dir /assets/network/yt-vad-autosubs-2500/yt-vad-autosubs-2500-export/ \
   --log_level 1 \
   --report_count 100 \
   --xla=False \
