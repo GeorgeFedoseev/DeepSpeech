f3b24cf75ad989dcd7e0571973147131011f27a2
George Fedoseev <george.fedoseev@me.com>
2018-04-24 07:39:01 +0300

    switch to 1024

--- bin/train-yt-vad-autosubs-rus.sh
+++ bin/train-yt-vad-autosubs-rus.sh
@@ -20,9 +20,9 @@ python -u DeepSpeech.py \
   --validation_step 1 \
   --dropout_rate 0.22 \
   --default_stddev 0.046875 \
-  --n_hidden 2048 \
-  --checkpoint_dir /assets/network/yt-vad-autosubs-2048/yt-vad-autosubs-2048-checkpoints/ \
-  --export_dir /assets/network/yt-vad-autosubs-2048/yt-vad-autosubs-2048-export/ \
+  --n_hidden 1024 \
+  --checkpoint_dir /assets/network/yt-vad-autosubs-1024/yt-vad-autosubs-1024-checkpoints/ \
+  --export_dir /assets/network/yt-vad-autosubs-1024/yt-vad-autosubs-1024-export/ \
   --log_level 1 \
   --report_count 100 \
   --xla=False \
