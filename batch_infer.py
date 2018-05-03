846d075e056da05b008cb63c3e9a83a7140733ab
George Fedoseev <george.fedoseev@me.com>
2018-05-03 23:30:33 +0300

    measure inference time

--- batch_infer.py
+++ batch_infer.py
@@ -4,6 +4,13 @@ import DeepSpeech
 
 import sys
 
+import time
+
 if __name__ == '__main__':
     DeepSpeech.initialize_globals()
-    DeepSpeech.do_single_file_inference(sys.argv[1])
+
+    for i in range(0, 20):
+        print("Inference %i" % (i))
+        start_time = time.time()
+        DeepSpeech.do_single_file_inference(sys.argv[1])
+        print("Inference %i took %.2f seconds" % (i, time.time() - start_time)
