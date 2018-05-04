29a2bc621274179b28c57053b00deeec15bdcea2
George Fedoseev <george.fedoseev@me.com>
2018-05-04 16:46:16 +0300

    separate session for each thread

--- infer.py
+++ infer.py
@@ -10,16 +10,23 @@ import time
 
 from util import text as text_utils
 
+global inputs, outputs
+inputs = None
+outputs = None
+
 def init():
     DeepSpeech.initialize_globals()
 
-def init_session():   
+def init_session():  
+    global inputs
+    global outputs 
 
     print('Use Language Model: %s' % str(DeepSpeech.FLAGS.infer_use_lm))
 
     session = tf.Session(config=DeepSpeech.session_config)
 
-    inputs, outputs = DeepSpeech.create_inference_graph(batch_size=1, use_new_decoder=DeepSpeech.FLAGS.infer_use_lm)
+    if inputs == None;
+        inputs, outputs = DeepSpeech.create_inference_graph(batch_size=1, use_new_decoder=DeepSpeech.FLAGS.infer_use_lm)
     # Create a saver using variables from the above newly created graph
     saver = tf.train.Saver(tf.global_variables())
     # Restore variables from training checkpoint
