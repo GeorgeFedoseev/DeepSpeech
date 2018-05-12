f49e7037a815c4481d5d6e84f188b44e1b77afe1
George Fedoseev <george.fedoseev@me.com>
2018-05-12 12:53:42 +0300

    added script to convert clean result csv to clean csv

---
+++ maintenance/dataset_csv_filter_clean.py
@@ -0,0 +1,66 @@
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+import csv
+
+SAMPLE_RATE = 16000
+BYTE_WIDTH = 2
+
+
+def filter_clean(in_csv_path, out_csv_path):
+    with open(in_csv_path, 'r') as in_f:
+        csv_reader = csv.reader(in_f)
+        all_rows = list(csv_reader)[1:] # skip header
+        
+        print "total rows: %i" % (len(all_rows))
+
+
+        # clean
+        clean_rows = []
+        for i, r in enumerate(all_rows):
+            try:
+                if int(r[3].strip()) == 1:
+                    clean_rows.append([r[0], r[1], r[2]]) # append without last filter info column
+            except Exception as ex:
+                print "bad row %i %s" % (i+2, str(ex))
+
+        print "clean rows: %i" % (len(clean_rows))
+        if len(all_rows) > 0:
+            print "clean rows percentage: %.1f%%" % (float(len(clean_rows))/len(all_rows)*100)
+
+
+        # calculate clean speech seconds
+        clean_speech_seconds = 0
+        found_files_count = 0
+        for r in clean_rows:
+            if os.path.exists(r[0]):
+                found_files_count += 1
+
+                file_size = os.path.getsize(r[0])
+                audio_length = float(wav_filesize)/SAMPLE_RATE/BYTE_WIDTH
+                clean_speech_seconds += audio_length
+
+
+        clean_speech_hours = clean_speech_seconds/3600
+
+        print "found files percentage: %.2f%%" % (float(found_files_count)/len(clean_rows)*100)
+        print "total clean speech hours: %.2f" % (clean_speech_hours)
+
+
+        with open(out_csv_path, 'w') as out_f:
+            csv_writer = csv.writer(out_f)
+            csv_writer.writerow(["wav_filename", "wav_filesize", "transcript"])
+            csv_writer.writerows(clean_rows)
+
+
+    pass
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print('USAGE: python dataset_csv_filter_clean.py <in_csv_path> <out_csv_path>')
+    else:
+        filter_clean(sys.argv[1], sys.argv[2])
