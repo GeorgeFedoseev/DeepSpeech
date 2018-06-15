d1d5f3d0c8181f4f3b15a760634261c214d6b0ee
George Fedoseev <george.fedoseev@me.com>
2018-06-15 16:41:41 +0300

    added create_language_model maintenance script

--- const.py
+++ const.py
@@ -1,21 +1,23 @@
 import os
 
-curr_dir_path = os.path.dirname(os.path.realpath(__file__))
+DEEP_SPEECH_PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-DATA_DIR = os.path.join(curr_dir_path, "data/")
+DATA_DIR = os.path.join(DEEP_SPEECH_PROJECT_ROOT_PATH, "data/")
 
 VIDEO_DATA_DIR = os.path.join(DATA_DIR, "media_data/")
 
 # DeepSpeech Model Setup
 DEEP_SPEECH_N_HIDDEN = 2048
 
-DEEP_SPEECH_CHECKPOINT_DIR = "/home/gosha/Projects/DeepSpeech/data/yt-vad-1k-2048-checkpoints/"
+DEEP_SPEECH_CHECKPOINT_DIR = "/network/yt-vad-1k-2048/yt-vad-1k-2048-checkpoints/"
 
 DEEP_SPEECH_ALPHABET_PATH = os.path.join(DATA_DIR, "alphabet.txt")
 
-DEEP_SPEECH_LM_PATH = os.path.join(DATA_DIR, "lm", "lm.binary")
-DEEP_SPEECH_TRIE_PATH = os.path.join(DATA_DIR, "lm", "trie")
+DEEP_SPEECH_LM_DIR_PATH = os.path.join(DATA_DIR, "lm")
+DEEP_SPEECH_ARPA_PATH = os.path.join(DEEP_SPEECH_LM_DIR_PATH, "words.arpa")
+DEEP_SPEECH_LM_PATH = os.path.join(DEEP_SPEECH_LM_DIR_PATH, "lm.binary")
+DEEP_SPEECH_TRIE_PATH = os.path.join(DEEP_SPEECH_LM_DIR_PATH, "trie")
 
 TRANSCRIBED_DATA_PATH  = os.path.join(DATA_DIR, "transcribed_data/")
 TRANSCRIBED_SPEECH_SQLITE_DB_PATH = os.path.join(TRANSCRIBED_DATA_PATH, "transcribed_speech.sqlite3")
---
+++ maintenance/create_language_model.py
@@ -0,0 +1,51 @@
+import os
+import sys
+current_dir_path = os.path.dirname(os.path.realpath(__file__))
+project_root_path = os.path.join(current_dir_path, os.pardir)
+sys.path.insert(0, project_root_path)
+
+import const
+
+import subprocess
+
+
+
+# /DeepSpeech/data/lm/kenlm/build/bin/lmplz --text /DeepSpeech/data/lm/vocabulary.txt --arpa /DeepSpeech/data/lm/words.arpa --o 3 --prune 0 2 2 -S 50%
+# /DeepSpeech/data/lm/kenlm/build/bin/build_binary -T -s /DeepSpeech/data/lm/words.arpa /DeepSpeech/data/lm/lm.binary
+# /DeepSpeech/native_client/generate_trie /DeepSpeech/data/alphabet.txt /DeepSpeech/data/lm/lm.binary /DeepSpeech/data/lm/vocabulary.txt /DeepSpeech/data/lm/trie
+
+def create_lm(vocabulary_path, o=5, prune=2):
+
+    lmplz_path = os.path.join(const.DEEP_SPEECH_LM_DIR_PATH, "kenlm/build/bin/lmplz")
+    build_binary_path = os.path.join(const.DEEP_SPEECH_LM_DIR_PATH, "kenlm/build/bin/build_binary")
+    generate_trie_path = os.path.join(const.DEEP_SPEECH_PROJECT_ROOT_PATH, "native_client/generate_trie")
+
+    prune_str = "0 "
+    for i in range(0, o-1):
+        prune_str += str(prune)+" "
+
+    p = subprocess.Popen([
+        "%s --text %s --arpa %s --o %i --prune %s -S 50%" % (lmplz_path, vocabulary_path, const.DEEP_SPEECH_ARPA_PATH, o, prune_str)
+        + " & %s -T -s %s %s" % (build_binary_path, const.DEEP_SPEECH_ARPA_PATH, const.DEEP_SPEECH_LM_PATH)
+        + " & %s %s %s %s" % (generate_trie_path, const.DEEP_SPEECH_ALPHABET_PATH, const.DEEP_SPEECH_LM_PATH, vocabulary_path, const.DEEP_SPEECH_TRIE_PATH)
+    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    out, err = p.communicate()
+
+    if p.returncode != 0:
+        raise Exception("Failed to loudnorm: %s" % str(err))
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        o = 5
+        prune = 2
+
+        if len(sys.argv) > 2:
+            o = int(sys.argv[2])
+
+        if len(sys.argv) > 3:
+            prune = int(sys.argv[3])
+
+        create_lm(sys.argv[1], o, prune)
+    else:
+        print "Usage: create_language_model.py <path-to-vocabulary-text-fie> [<o - max word seq len>] [<prune - min number of word occurences>]"
