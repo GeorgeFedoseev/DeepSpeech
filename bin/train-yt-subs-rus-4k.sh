0c9a0b17239bee4dcf857b430bead75b02f3afe3
George Fedoseev <george.fedoseev@me.com>
2018-06-24 20:00:54 +0300

    training script changes

--- bin/train-yt-subs-rus-4k.sh
+++ bin/train-yt-subs-rus-4k.sh
@@ -16,7 +16,7 @@ python -u DeepSpeech.py \
   --test_batch_size 16 \
   --learning_rate 0.0001 \
   --epoch 35 \
-  --display_step 0 \
+  --display_step 5 \
   --validation_step 1 \
   --dropout_rate 0.3 \
   --default_stddev 0.046875 \
@@ -24,8 +24,10 @@ python -u DeepSpeech.py \
   --checkpoint_dir /assets/network/yt-subs-rus-4k-2048/yt-subs-rus-4k-2048-checkpoints/ \
   --export_dir /assets/network/yt-subs-rus-4k-2048/yt-subs-rus-4k-2048-export/ \
   --log_level 1 \
-  --report_count 100 \
+  --report_count 0 \
   --xla=False \
   --use_warpctc=True \
   --log_telegram=True \
+  --limit_dev 5000 \
+  --limit_test 5000 \
   "$@"
